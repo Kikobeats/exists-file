6a0c818454e137a4a36c651a9b391773f462e22f
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-04-17 23:29:33 +0200

    Update scaffold

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
-'use strict';
+'use strict'
 
-var fs = require('fs');
-var path = require('path');
+var fs = require('fs')
+var path = require('path')
 
-var getAbsolutePath = function(filepath) {
-  return path.resolve(process.cwd(), filepath);
-};
+var getAbsolutePath = function (filepath) {
+  return path.resolve(process.cwd(), filepath)
+}
 
 var methods = {
-
-  async: function(filepath, cb) {
-    return fs.stat(getAbsolutePath(filepath), function(err, stats) {
-      if (err == null) return cb(null, true);
-      if (err.code === 'ENOENT') return cb(null, false);
-      return cb(err, stats);
-    });
-
+  async: function (filepath, cb) {
+    return fs.stat(getAbsolutePath(filepath), function (err, stats) {
+      if (err == null) return cb(null, true)
+      if (err.code === 'ENOENT') return cb(null, false)
+      return cb(err, stats)
+    })
   },
 
-  sync: function(filepath) {
+  sync: function (filepath) {
     try {
-      fs.statSync(getAbsolutePath(filepath));
-      return true;
+      fs.statSync(getAbsolutePath(filepath))
+      return true
     } catch (err) {
-      if (err.code === 'ENOENT') return false;
-      throw err;
+      if (err.code === 'ENOENT') return false
+      throw err
     }
   }
-};
+}
 
-module.exports = function(filepath, cb) {
-  var method = cb ? 'async' : 'sync';
+module.exports = function (filepath, cb) {
+  var method = cb ? 'async' : 'sync'
   if (typeof filepath !== 'string') {
-    if (cb) return cb(null, false);
+    if (cb) return cb(null, false)
     return false
   }
-  return methods[method].apply(this, arguments);
-};
+  return methods[method].apply(this, arguments)
+}
